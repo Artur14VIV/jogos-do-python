5c07d0de5fc99d53277b4d55578ee43443940e89
Artur14VIV <artur.fritzen@escola.pr.gov.br>
2026-04-06 09:23:53 -0300

    Update forca.py
    coloquei uma pequena demonstração de uma pessoa em uma forca.

--- forca.py
+++ forca.py
@@ -37,17 +37,34 @@ def jogar():
        else:
            erros += 1
 
+       if erros == 1:
+           print("O")
+       elif erros == 2:
+           print("0")
+           print("|")
+       if erros == 3:  # <-- Aqui começa um novo 'if'
+           print(" 0")
+           print("/|")
+       elif erros == 4:
+           print(" 0")
+           print("/||")
+       if erros == 5:
+           print(" 0")
+           print("/||")
+           print(" /  ")
+       elif erros == 6:
+           print("  0")
+           print(" /||")
+           print(" /|")
 
        enforcou = erros == 6
        print(letras_acertadas)
        acertou = not "_" in letras_acertadas
 
-
-   if enforcou == True:
-       print("Fim de jogo, você morreu")
-       return
-   elif not "_" in letras_acertadas:
-       print("Fim do jogo, você acertou")
-       return
+       if enforcou == True:
+          print("Fim de jogo, você morreu")
+          return
+       elif not "_" in letras_acertadas:
+         print("Fim do jogo, você acertou")
+         return
 jogar()
-
